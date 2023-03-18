2beb20df509a006c2d7e9c0a6144aa7db7b6fc5c
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-03-17 23:24:41 -0700

    [update] minor change.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,13 +8,14 @@ import { MdTravelExplore } from 'react-icons/md'
 const Navbar = () => {
     return (
       <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-[#0E7373]'>
+        
         <div className='w-full text-[#0E7373] flex px-1 space-x-4'>
         <GiFilmStrip size={20}/>
         <BiCameraMovie size={20}/>
         <ImHeadphones size={20}/>
         <MdTravelExplore size={20}/>
         </div>
-        <ul className='hidden md:flex'>
+        <ul className='md:flex'>
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 p-4 text-[#0E7373]'>
         <NavLink 
 	            className={({ isActive }) => isActive ? 'nav-link-active' : 'nav-link' }
