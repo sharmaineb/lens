128c3b6cdb6fd286cc4e7c73b19e58ad5152451f
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-03-22 18:23:18 -0700

    [update]

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -6,7 +6,7 @@ import data from '../../filmdata'
 function Details(props) {
   const params = useParams()
   const { id } = params // Location index
-  const { images, title, desc, features, song, album, movie, musician } = data[id]
+  const { images, title, desc, features, song, album, movieimg, musician, movie } = data[id]
 
   return (
     <><div className='w-full bg-[#fff] py-15 px-1 mx-auto my-auto p-1'>
@@ -34,10 +34,12 @@ function Details(props) {
               <img className='w-[450px] mx-auto my-5 px-7' src={`${process.env.PUBLIC_URL}/images/${features[0]}`} alt={title} />
               </div>
           </div>
-          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-              <p className='text-center text-3xl font-bold'>Lorem Ipsum</p>
+          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 justify-center'>
+              <p className='text-center text-3xl font-bold'>Movie: </p>
               <div className='text-center font-medium'>
-              <img className='w-[450px] mx-auto my-5 px-7' src={`${process.env.PUBLIC_URL}/images/${movie[0]}`} alt={title} />
+              <p className='py-2 border-b mx-8 mt-8'>{movie}</p>
+              <img className='w-[450px] mx-auto my-5 px-7' src={`${process.env.PUBLIC_URL}/images/${movieimg[0]}`} alt={title} />
+              {/* <button className='bg-[#F2E63D] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Here's A Button?</button> */}
               </div>
           </div>
       </div>
