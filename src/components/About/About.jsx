ef32d15f516c44d7f78aa125807a7b8546f0a7ff
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-03-22 21:15:23 -0700

    [update] set-up about page.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,66 @@
 import React from "react";
+import Lightbulb from '../../assets/lightbulb.jpg'
+import Camera from '../../assets/aboutcamera.jpg'
+import Photos from '../../assets/aboutphotos.jpg'
+import Movies from '../../assets/movies.jpg'
+import Headphones from '../../assets/aboutheadphones.jpg'
+import Bay from '../../assets/yee.jpg'
+import Inspire from '../../assets/inspired.jpg'
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Welcome</p>
+    <div className="">
+      <img className='w-[950px] mx-auto my-5 px-3' src={Lightbulb} alt='/' />
+      <p className="pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer viverra pellentesque orci, eget sagittis risus bibendum sit amet. 
+         Morbi commodo blandit dictum. Donec volutpat ex a metus cursus sollicitudin. 
+         Quisque a facilisis nulla, sed hendrerit massa. Nam dapibus, ligula nec blandit 
+         volutpat, arcu lectus vestibulum augue, semper congue arcu quam nec lacus. 
+         Maecenas eu libero elit. Etiam at vestibulum velit, et euismod orci. 
+         Nam id massa ac leo blandit ultrices sed at leo. Fusce bibendum lobortis eleifend.</p>
+
+        {/* -------------------------  */}
+        <img className='w-[950px] mx-auto my-5 px-3' src={Camera} alt='/' />
+        <p className="pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer viverra pellentesque orci, eget sagittis risus bibendum sit amet. 
+         Morbi commodo blandit dictum. Donec volutpat ex a metus cursus sollicitudin. 
+         Quisque a facilisis nulla, sed hendrerit massa. Nam dapibus, ligula nec blandit 
+         volutpat, arcu lectus vestibulum augue, semper congue arcu quam nec lacus. 
+         Maecenas eu libero elit. Etiam at vestibulum velit, et euismod orci. 
+         Nam id massa ac leo blandit ultrices sed at leo. Fusce bibendum lobortis eleifend.</p>
+        <img className='w-[950px] mx-auto my-5 px-3' src={Photos} alt='/' />
+        <p className="pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer viverra pellentesque orci, eget sagittis risus bibendum sit amet. 
+         Morbi commodo blandit dictum. Donec volutpat ex a metus cursus sollicitudin. 
+         Quisque a facilisis nulla, sed hendrerit massa. Nam dapibus, ligula nec blandit 
+         volutpat, arcu lectus vestibulum augue, semper congue arcu quam nec lacus. 
+         Maecenas eu libero elit. Etiam at vestibulum velit, et euismod orci. 
+         Nam id massa ac leo blandit ultrices sed at leo. Fusce bibendum lobortis eleifend.</p>
+        <img className='w-[950px] mx-auto my-5 px-3' src={Movies} alt='/' />
+        <p className="pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer viverra pellentesque orci, eget sagittis risus bibendum sit amet. 
+         Morbi commodo blandit dictum. Donec volutpat ex a metus cursus sollicitudin. 
+         Quisque a facilisis nulla, sed hendrerit massa. Nam dapibus, ligula nec blandit 
+         volutpat, arcu lectus vestibulum augue, semper congue arcu quam nec lacus. 
+         Maecenas eu libero elit. Etiam at vestibulum velit, et euismod orci. 
+         Nam id massa ac leo blandit ultrices sed at leo. Fusce bibendum lobortis eleifend.</p>
+        <img className='w-[950px] mx-auto my-5 px-3' src={Headphones} alt='/' />
+        <p className="pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer viverra pellentesque orci, eget sagittis risus bibendum sit amet. 
+         Morbi commodo blandit dictum. Donec volutpat ex a metus cursus sollicitudin. 
+         Quisque a facilisis nulla, sed hendrerit massa. Nam dapibus, ligula nec blandit 
+         volutpat, arcu lectus vestibulum augue, semper congue arcu quam nec lacus. 
+         Maecenas eu libero elit. Etiam at vestibulum velit, et euismod orci. 
+         Nam id massa ac leo blandit ultrices sed at leo. Fusce bibendum lobortis eleifend.</p>
+        <img className='w-[950px] mx-auto my-5 px-3' src={Bay} alt='/' />
+        <p className="pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer viverra pellentesque orci, eget sagittis risus bibendum sit amet. 
+         Morbi commodo blandit dictum. Donec volutpat ex a metus cursus sollicitudin. 
+         Quisque a facilisis nulla, sed hendrerit massa. Nam dapibus, ligula nec blandit 
+         volutpat, arcu lectus vestibulum augue, semper congue arcu quam nec lacus. 
+         Maecenas eu libero elit. Etiam at vestibulum velit, et euismod orci. 
+         Nam id massa ac leo blandit ultrices sed at leo. Fusce bibendum lobortis eleifend.</p>
+        <img className='w-[950px] mx-auto my-5 px-3' src={Inspire} alt='/' />
+        <p className="pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer viverra pellentesque orci, eget sagittis risus bibendum sit amet. 
+         Morbi commodo blandit dictum. Donec volutpat ex a metus cursus sollicitudin. 
+         Quisque a facilisis nulla, sed hendrerit massa. Nam dapibus, ligula nec blandit 
+         volutpat, arcu lectus vestibulum augue, semper congue arcu quam nec lacus. 
+         Maecenas eu libero elit. Etiam at vestibulum velit, et euismod orci. 
+         Nam id massa ac leo blandit ultrices sed at leo. Fusce bibendum lobortis eleifend.</p>
     </div>
   )
 }
