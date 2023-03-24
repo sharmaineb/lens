500935be9a8da5d9cbc56c83fa231d9311c62177
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-03-24 10:21:38 -0700

    [update]

--- src/components/Random/Random.jsx
+++ src/components/Random/Random.jsx
@@ -5,7 +5,6 @@ function RandomSpace() {
 	const navigate = useNavigate()
   return (
 		<button 
-    className="RandomSpace"
     onClick={(e) => {
 			const id = Math.floor(Math.random() * data.length)
 			navigate(`/details/${id}`)
